c429bad98756effc2c674efa6866a5a9db4c757e
Slenzy <137514443+SlenzyCode@users.noreply.github.com>
2023-12-23 19:58:20 +0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const slenzycode = require("slenzycode /* Buraya kendi modül adınızı giriniz package.json name'in karşısına yazdığınız*/");
+const slenzycode = require("slenzycode" /* Buraya kendi modül adınızı giriniz package.json name'in karşısına yazdığınız*/);
 
 slenzycode.gpt("sa").then(response => {
     console.log(response);
